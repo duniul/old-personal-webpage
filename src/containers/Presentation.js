7a30c0f7d688ce59c9bf70b936b1de9c1fed3aaa
Daniel <daniel.gronstrand@gmail.com>
2018-01-25 00:13:17 +0100

    Improve responsiveness

--- src/containers/Presentation.js
+++ src/containers/Presentation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
-import { Button, Container, Grid } from 'semantic-ui-react';
+import { Button, Grid } from 'semantic-ui-react';
 import Avatar from '../components/Avatar';
 import SpeechBubble from '../components/SpeechBubble';
 import './presentation.css';
@@ -21,53 +21,48 @@ class Presentation extends React.PureComponent {
 
     render() {
         const { showTLDR } = this.state;
-        const socialIconProps = {
-            style: { height: 40, width: 40, marginLeft: '0.2em' }
-        };
 
         return (
             <div className="presentation">
-                <Container style={{ paddingTop: '8em' }}>
-                    <Grid relaxed stackable verticalAlign="middle">
-                        <Grid.Row>
-                            <Grid.Column width={4}>
-                                <Avatar happy={!showTLDR} />
-                            </Grid.Column>
-                            <Grid.Column width={12}>
-                                <SpeechBubble>
-                                    <h1 className="presentation-header">
-                                        Hello, I'm Daniel
-                                        <div style={{ float: 'right', marginTop: '-0.1em' }}>
-                                            <SocialIcon {...socialIconProps} url="mailto:daniel@gronstrand.com" color="#ef5350" />
-                                            <SocialIcon {...socialIconProps} url="https://www.linkedin.com/in/danielgronstrand/" />
-                                            <SocialIcon {...socialIconProps} url="https://github.com/gronstrand/" color="#24292e" />
-                                        </div>
-                                    </h1>
-                                    <div className={`presentation-text ${showTLDR ? 'tldr' : ''}`}>
-                                        <p style={{ marginBottom: '0.4em' }}>
-                                            A <span className="highlighted">Swedish</span> man who likes to tinker with things, like code, hardware and/or Legos.
-                                            I currently work as a <span className="highlighted">full-stack developer</span> at Com Hem <span className="highlighted">in Stockholm</span>.
-                                        </p>
-                                        <p>
-                                            I love to create things and challenge myself, like trying out odd new programming languages,
-                                            even though I am most <span className="highlighted">proficient in</span> mainstream languages
-                                            like <span className="highlighted">JavaScript</span> and <span className="highlighted">Java</span>.
-                                        </p>
+                <Grid relaxed stackable verticalAlign="middle">
+                    <Grid.Row>
+                        <Grid.Column width={4}>
+                            <Avatar happy={!showTLDR} />
+                        </Grid.Column>
+                        <Grid.Column width={12}>
+                            <SpeechBubble>
+                                <h1 className="presentation-header">
+                                    Hello, I'm Daniel
+                                    <div className="social-links">
+                                        <SocialIcon url="mailto:daniel@gronstrand.com" color="#ef5350" />
+                                        <SocialIcon url="https://www.linkedin.com/in/danielgronstrand/" />
+                                        <SocialIcon url="https://github.com/gronstrand/" color="#24292e" />
                                     </div>
-                                    <Button
-                                        className="tldr-button"
-                                        active={showTLDR}
-                                        toggle
-                                        size="big"
-                                        onClick={this.onClickTLDR}
-                                    >
-                                        {showTLDR ? 'Give me the deets!' : 'TL;DR'}
-                                    </Button>
-                                </SpeechBubble>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Container>
+                                </h1>
+                                <div className={`presentation-text ${showTLDR ? 'tldr' : ''}`}>
+                                    <p style={{ marginBottom: '0.4em' }}>
+                                        A <span className="highlighted">Swedish</span> man who likes to tinker with things, like code, hardware and/or Legos.
+                                        I currently work as a <span className="highlighted">full-stack developer</span> at Com Hem <span className="highlighted">in Stockholm</span>.
+                                    </p>
+                                    <p>
+                                        I love to create things and challenge myself, like trying out odd new programming languages,
+                                        even though I am most <span className="highlighted">proficient in</span> mainstream languages
+                                        like <span className="highlighted">JavaScript</span> and <span className="highlighted">Java</span>.
+                                    </p>
+                                </div>
+                                <Button
+                                    className="tldr-button"
+                                    active={showTLDR}
+                                    toggle
+                                    size="big"
+                                    onClick={this.onClickTLDR}
+                                >
+                                    {showTLDR ? 'Give me the deets!' : 'TL;DR'}
+                                </Button>
+                            </SpeechBubble>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </div>
         );
     }
