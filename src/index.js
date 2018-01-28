81388db0f6970989f4525392635f5c31c7327214
Daniel <daniel.gronstrand@gmail.com>
2018-01-29 00:02:55 +0100

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import Main from './Main';
 import './main.css';
 import configureStore from './redux/configureStore';
@@ -10,6 +11,11 @@ const rootElement = document.getElementById('root');
 const renderApp = AppComponent => {
     ReactDOM.render(<AppComponent store={store} />, rootElement);
 };
+
+if (process.env.NODE_ENV === 'production') {
+    ReactGA.initialize('UA-000000-01');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 renderApp(Main);
 registerServiceWorker();
