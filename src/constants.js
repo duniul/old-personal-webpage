ef52ce8eec31c180b8db2dc29f2fe8e7b2cf7ea3
Daniel Grefberg <hello@danielgrefberg.com>
2020-08-02 20:30:25 +0200

    Update speech bubble texts

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,8 @@ export const urls = {
   SETTLER_RENAMING: 'https://www.nexusmods.com/fallout4/mods/2017',
   VOT: 'https://www.nexusmods.com/fallout4/mods/2173',
   AWKCR: 'https://www.nexusmods.com/fallout4/mods/6091',
-  GAME_ENGINE: 'https://github.com/gronstrand/sp6-vt2016-gameengine',
-  HADOUKATT: 'https://github.com/gronstrand/sp6-vt2016-hadoukatt',
-  GRONSTRAND_COM_GITHUB: 'https://github.com/gronstrand/gronstrand-com',
+  GAME_ENGINE: 'https://github.com/duniul/archive',
+  HADOUKATT: 'https://github.com/duniul/archive',
+  WEBPAGE_REPO: 'https://github.com/duniul/old-personal-webpage',
+  GITHUB: 'https://github.com/duniul',
 };
