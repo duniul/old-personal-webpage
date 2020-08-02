34ac1ac6ecf0ae48101fac9ab7fce320f3d44e68
Daniel Grefberg <hello@danielgrefberg.com>
2020-08-02 20:30:25 +0200

    Update speech bubble texts

--- src/components/SpeechBubble.js
+++ src/components/SpeechBubble.js
@@ -43,7 +43,7 @@ class SpeechBubble extends React.Component {
     setTimeout(() => this.setState({ faded: false }), 600);
   }
 
-  onClickOption = path => () => {
+  onClickOption = (path) => () => {
     if (path === this.props.location.pathname) {
       return;
     }
@@ -71,7 +71,7 @@ class SpeechBubble extends React.Component {
     this.props.toggleTldr();
   };
 
-  renderHeader = title => {
+  renderHeader = (title) => {
     return (
       <h1 className="presentation-header">
         {title}
@@ -92,12 +92,12 @@ class SpeechBubble extends React.Component {
         title: 'Who are you again?',
       },
       {
-        path: paths.MODS,
-        title: 'Tell me more about those mods.',
-      },
-      {
         path: paths.PROJECTS,
         title: 'What else have you done?',
+      },
+      {
+        path: paths.MODS,
+        title: 'Tell me more about those mods.',
       },
     ];
 
@@ -105,17 +105,19 @@ class SpeechBubble extends React.Component {
       <div className="options">
         <Divider />
         <Button.Group fluid vertical compact>
-          {options.filter(option => option.path !== this.props.location.pathname).map(option => (
-            <Button
-              key={option.path}
-              size="big"
-              onClick={this.onClickOption(option.path)}
-              style={{ marginBottom: 4 }}
-            >
-              -&ensp;
-              {option.title}
-            </Button>
-          ))}
+          {options
+            .filter((option) => option.path !== this.props.location.pathname)
+            .map((option) => (
+              <Button
+                key={option.path}
+                size="big"
+                onClick={this.onClickOption(option.path)}
+                style={{ marginBottom: 4 }}
+              >
+                -&ensp;
+                {option.title}
+              </Button>
+            ))}
 
           <Button
             className="tldr-button"
@@ -140,45 +142,44 @@ class SpeechBubble extends React.Component {
         {this.renderHeader('Other projects')}
         <div className={`presentation-text ${tldr ? 'tldr' : ''}`}>
           <p>
-            Well, during my time at Com Hem I built a{' '}
-            <Highlight color="pink">web based, in-house CRM-system</Highlight> from scratch,
-            together with four other people. It's a modern, fast SPA built with React.js, Java and
+            During my time at Com Hem, I built a{' '}
+            <Highlight color="pink">web-based, in-house CRM-system</Highlight> from scratch,
+            together with four other people. It's a modern, fast SPA built with React.js, Java, and
             Oracle. The system has been my primary focus at work.
           </p>
           <p>
             I have also been a part of several hackathons and side projects at Com Hem. Together
-            with some colleagues I built an online,{' '}
-            <Highlight color="orange">interactive map</Highlight> of the company office to help new
-            and/or confused employees find their way around. We also created a{' '}
-            <Highlight color="orange">web service</Highlight> for customers to{' '}
-            <Highlight color="orange">easily configure modems</Highlight>, skipping the hazzle of
-            having to manually log into it.
+            with some, I built an online, <Highlight color="orange">interactive map</Highlight> of
+            the company office to help new or confused employees find their way around. We also
+            created a <Highlight color="orange">web service</Highlight> for customers to{' '}
+            <Highlight color="orange">easily configure modems</Highlight>, skipping the hassle of
+            manually logging in to it.
           </p>
           <p>
-            All of these projects were made at and for Com Hem, so I am sadly not allowed to share
-            any in-depth information or source code for them. However, there are others that I have
-            been working on during my spare time which I am able to share.
+            These projects were all done at and for Com Hem, so I am sadly not able to share any
+            in-depth information or source code. However, there are other things that I have been
+            working on during my spare time, which I can share.
           </p>
           <p>
             I{' '}
             <Highlight color="blue" url={urls.GAME_ENGINE}>
               wrote a game engine
             </Highlight>{' '}
-            from scratch in Java, and{' '}
+            from scratch in Java and{' '}
             <Highlight color="blue" url={urls.HADOUKATT}>
               created a game with it
             </Highlight>
-            . The game is like a poor man's Duck Game where you play as a cat shooting other cats{' '}
+            . The game is like a poor man's Duck Game, where you play as a cat shooting other cats{' '}
             with <img src={Hadouken} alt="" style={{ height: '0.8em' }} /> hadoukens. Since I only
-            had about two weeks to make both the engine and the game, I didn't have time polish it
-            enough. Maybe I'll pick it up again some day?
+            had about two weeks to make both the engine and the game, I didn't have time to polish
+            it enough. Maybe I'll pick it up again some day?
           </p>
           <p>
-            Finally, there's{' '}
-            <Highlight color="blue" url={urls.GRONSTRAND_COM_GITHUB}>
-              this website
+            You can find more of the stuff I've made on my{' '}
+            <Highlight color="blue" url={urls.GITHUB}>
+              Github profile
             </Highlight>
-            . It's a new project of mine, and hopefully it will grow into something nice.
+            .
           </p>
         </div>
       </React.Fragment>
@@ -194,20 +195,24 @@ class SpeechBubble extends React.Component {
           <p>
             If there is something I feel like I am missing in a game, you can be sure that I will
             try to either find a way to fix it, or create one on my own. I usually don't release
-            them, but I made some mods for Fallout 4 that I thought could be useful to others.
+            them, but I made some for <i>Fallout 4</i> and <i>Mount and Blade: Bannerlord</i> that I
+            thought could be useful to others. I publish most of them on{' '}
+            <Highlight color="orange" url={urls.NEXUS}>
+              NexusMods
+            </Highlight>
+            .
           </p>
 
           <p>
-            The most popular one is{' '}
             <Highlight color="teal" url={urls.CRAFTING_WORKBENCHES}>
               Crafting Workbenches
-            </Highlight>
-            , which allows players to <Highlight color="teal">craft items</Highlight> from
-            mater&shy;ials. It also serves as a{' '}
+            </Highlight>{' '}
+            is the most popular one that I made on my own, which allows players to{' '}
+            <Highlight color="teal">craft items</Highlight> from mater&shy;ials. It also serves as a{' '}
             <Highlight color="teal">plat&shy;form for modders</Highlight> to make their own
             creations craft&shy;able in the game. It is currently in the top 30 of the most endorsed
-            Fallout 4 mods of all time, with over a{' '}
-            <Highlight color="teal">million downloads</Highlight>.{' '}
+            Fallout 4 mods of all time, with{' '}
+            <Highlight color="teal">over a million downloads</Highlight>.{' '}
           </p>
 
           <p>
@@ -218,22 +223,22 @@ class SpeechBubble extends React.Component {
             creations compatible with each other. I joined the project in the early stages and{' '}
             <Highlight color="pink">contributed code</Highlight> from Crafting Workbenches. I also
             created many of its community patches. It is currently the{' '}
-            <Highlight color="pink">most downloaded</Highlight> Fallout 4 mod of all time.
+            <Highlight color="pink">most downloaded Fallout 4 mod of all time</Highlight>.
           </p>
 
           <p>
-            I also created a number of <Highlight color="orange">smaller mods</Highlight>. One lets
+            I also created a number of <Highlight color="blue">smaller mods</Highlight>. One lets
             players{' '}
-            <Highlight color="orange" url={urls.COMPANION_TRACKING}>
+            <Highlight color="blue" url={urls.COMPANION_TRACKING}>
               track companions
             </Highlight>
             , a feature which was later adopted by the game developers themselves. Another one
             allows players to{' '}
-            <Highlight color="orange" url={urls.SETTLER_RENAMING}>
+            <Highlight color="blue" url={urls.SETTLER_RENAMING}>
               rename characters
             </Highlight>{' '}
             in the game using simple batch commands. Finally, one lets players{' '}
-            <Highlight color="orange" url={urls.VOT}>
+            <Highlight color="blue" url={urls.VOT}>
               aim great distances
             </Highlight>{' '}
             with the games assisted aiming interface, V.A.T.S.
@@ -247,25 +252,31 @@ class SpeechBubble extends React.Component {
     const { tldr } = this.props;
     return (
       <React.Fragment>
-        {this.renderHeader("Hi, I'm Daniel")}
+        {this.renderHeader("Hello, I'm Daniel")}
         <div className={`presentation-text ${tldr ? 'tldr' : ''}`}>
           <p>
-            A <Highlight color="blue">Swedish</Highlight> man who likes to tinker with things, like
-            code, hard&shy;ware or Legos. I currently work as a{' '}
-            <Highlight color="blue">full-stack developer</Highlight> at Com Hem, in{' '}
-            <Highlight color="blue">Stockholm</Highlight>.
+            I'm a <Highlight color="blue">Swedish</Highlight> developer who likes to tinker with
+            things like code, hard&shy;ware, and Legos. I currently work as a{' '}
+            <Highlight color="blue">Senior Software Engineer</Highlight> at{' '}
+            <Highlight color="blue">Acast</Highlight> in{' '}
+            <Highlight color="blue">Stockholm</Highlight>. Before that, I was at Com Hem as a
+            full-stack developer.
           </p>
           <p>
-            I love to create things and challenge myself, like trying out odd new programming
-            languages, even though I am most <Highlight color="orange">proficient in</Highlight>{' '}
-            main&shy;stream languages like <Highlight color="orange">JavaScript</Highlight> and{' '}
-            <Highlight color="orange">Java</Highlight>.
+            I think it's fair to say I'm{' '}
+            <Highlight color="orange">proficient in JavaScript</Highlight> by now, having worked
+            full-time with <Highlight color="orange">React</Highlight>,{' '}
+            <Highlight color="orange">Node</Highlight>, and{' '}
+            <Highlight color="orange">TypeScript</Highlight> for several years. Before that, I did{' '}
+            <Highlight color="orange">quite a bit of Java</Highlight>, and I love to try out new
+            languages and frame&shy;works in general.
           </p>
           <p>
-            I am also an avid <Highlight color="teal">gamer</Highlight>, and have{' '}
-            <Highlight color="teal">dabbled in game modding</Highlight> as a hobby. I published a
-            couple of the ones I made for Fallout 4, and they now have over a{' '}
-            <Highlight color="teal">million downloads</Highlight> and counting.
+            I got into prog&shy;ramming as a hobbyist{' '}
+            <Highlight color="teal">game modder</Highlight>, and I still mod the games I play now
+            and then. Some of <Highlight color="teal">my mods</Highlight> have been more
+            success&shy;ful than others, like my Fallout 4 mods that now{' '}
+            <Highlight color="teal">have millions of downloads</Highlight>.
           </p>
         </div>
       </React.Fragment>
